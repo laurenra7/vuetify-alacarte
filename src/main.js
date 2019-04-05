7b5142e80c76d6a7c776c3efdb33272ee6755521
Lauren Anderson <laurenra@byu.edu>
2019-04-05 12:51:52 -0600

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Method 1: default, using plugin
+// import './plugins/vuetify'
+
+// Method 2: only specific components in main.js
+// import Vuetify, {
+//   VApp, // required
+//   VBtn
+// } from 'vuetify/lib'
+//
+// Vue.use(Vuetify, {
+//   components: {
+//     VApp,
+//     VBtn
+//   }
+// })
+
+// Method 3: vuetify-loader
+// import Vuetify from 'vuetify/lib'
+//
+// Vue.use(Vuetify)
+
 Vue.config.productionTip = false
 
 new Vue({
